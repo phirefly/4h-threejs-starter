6725aa1d23d1730cc301b6edd47b6c6ac2719516
Raphael Villas <raphael.villas@gmail.com>
2024-02-10 13:39:50 -0600

    Box from class 2

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ import * as THREE from 'three';
 console.log("Hello World!");
 console.log(THREE);
 
+const scene = new THREE.Scene();
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000
+});
 
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+
+scene.add(cubeMesh);
+
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 20;
+camera.position.x = 0;
+camera.position.y = -5;
+
+
+const canvas = document.querySelector('canvas.threejs-canvas');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
 
 
